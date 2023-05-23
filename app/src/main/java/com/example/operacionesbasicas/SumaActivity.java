5a9da825922da51c4067624645ea70cb415734a9
Michaelbadango <126106195+Michaelbadango@users.noreply.github.com>
2023-05-22 19:01:28 -0500

    Suma

--- app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/SumaActivity.java
@@ -3,6 +3,9 @@ package com.example.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class SumaActivity extends AppCompatActivity {
 
@@ -10,5 +13,18 @@ public class SumaActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_suma);
+    }
+
+    public void regresar(View view){
+        this.finish();
+    }
+
+    public void sumar(View view){
+        EditText N1 = (EditText) findViewById(R.id.txt_n1);
+        EditText N2 = (EditText) findViewById(R.id.txt_n2);
+        TextView re = findViewById(R.id.txt_r);
+        double resultado = Double.parseDouble( N1.getText().toString()) + Double.parseDouble( N2.getText().toString());
+
+        re.setText(resultado+"");
     }
 }
