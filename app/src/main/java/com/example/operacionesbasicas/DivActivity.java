83f1ebbc4f4e60ede6e308d9b8accc0d37fe7525
Michaelbadango <126106195+Michaelbadango@users.noreply.github.com>
2023-05-22 20:13:28 -0500

    Operacion Division

--- app/src/main/java/com/example/operacionesbasicas/DivActivity.java
+++ app/src/main/java/com/example/operacionesbasicas/DivActivity.java
@@ -3,6 +3,9 @@ package com.example.operacionesbasicas;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class DivActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class DivActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_div);
+    }
+
+    public void regresar(View view){
+        this.finish();
+    }
+
+    public void Dividir(View view){
+        EditText N1 = (EditText) findViewById(R.id.txt_num1);
+        EditText N2 = (EditText) findViewById(R.id.txt_num2);
+        TextView re = findViewById(R.id.total);
+        double resultado = Double.parseDouble( N1.getText().toString()) / Double.parseDouble( N2.getText().toString());
+        re.setText(resultado+"");
     }
 }
